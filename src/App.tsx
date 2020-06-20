b1cee4778b0ed62adf5a0956a53f40035a78cd07
CamilleSylla <camillesylla.web@gmail.com>
2020-06-20 18:37:45 +0200

    Set CurrentView to month

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import { Inject, ScheduleComponent, Day, Week, WorkWeek, Month, TimelineYear, Agenda } from '@syncfusion/ej2-react-schedule';
+import { Inject, ScheduleComponent, Day, Week, Month, Agenda,  } from '@syncfusion/ej2-react-schedule';
 
 
 class App extends React.Component {
   public render () {
-    return <ScheduleComponent>
-      <Inject services={[ Day, Week, WorkWeek, Month, Agenda, TimelineYear]} />
+    return <ScheduleComponent currentView="Month" selectedDate={new Date(2020, 0, 11)}>
+      <Inject services={[ Day, Week, Month, Agenda,]} />
     </ScheduleComponent>
   }
 }
