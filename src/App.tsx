b48b8096c461676a307f2b3140f3411cac8f8b46
CamilleSylla <camillesylla.web@gmail.com>
2020-06-21 23:22:24 +0200

    Added new folder and link Schedule fonctionnality in the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import './App.css';
-import { Inject, ScheduleComponent, Day, Week, Month, Agenda,  } from '@syncfusion/ej2-react-schedule';
-
+import Calendrier from './calendrier/Schedule';
 
 class App extends React.Component {
-  public render () {
-    return <ScheduleComponent currentView="Month" selectedDate={new Date(2020, 0, 11)}>
-      <Inject services={[ Day, Week, Month, Agenda,]} />
-    </ScheduleComponent>
+  render (){
+    return (
+      <div> <Calendrier/> </div>
+    );
   }
 }
 
---
+++ src/calendrier/Schedule.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { Inject, ScheduleComponent, Day, Week, Month, Agenda,  } from '@syncfusion/ej2-react-schedule';
+
+
+class Calendrier extends React.Component {
+  public render () {
+    return <ScheduleComponent currentView="Month" selectedDate={new Date(2020, 0, 11)}>
+      <Inject services={[ Day, Week, Month, Agenda,]} />
+    </ScheduleComponent>
+  }
+}
+
+export default Calendrier;
