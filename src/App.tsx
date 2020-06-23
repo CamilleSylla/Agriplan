f22859a4e52d4a1fd9def5552978b16f17d82f4b
CamilleSylla <camillesylla.web@gmail.com>
2020-06-23 23:35:24 +0200

    added GSTC calendar to project end added it on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Calendrier from './calendrier/Schedule';
+import Plan from './gantsch/plan';
 
 class App extends React.Component {
   render (){
     return (
-      <div> <Calendrier/> </div>
+      <div> 
+        
+        <Calendrier/> 
+        <Plan/> 
+        
+        </div>
     );
   }
 }
